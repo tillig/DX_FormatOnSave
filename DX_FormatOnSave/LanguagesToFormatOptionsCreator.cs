1f021f77671998f103e460d8f9b53e080047f88d
Travis Illig <tillig@paraesthesia.com>
2016-08-29 14:24:01 -0700

    Options using the new observable mechanism.

---
+++ DX_FormatOnSave/LanguagesToFormatOptionsCreator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using DevExpress.CodeRush.Platform.Options;
+
+namespace DX_FormatOnSave
+{
+	/// <summary>
+	/// Creator for default options value for languages to format.
+	/// </summary>
+	public class LanguagesToFormatOptionsCreator : IOptionDefaultValueProcessor
+	{
+		/// <summary>
+		/// Creates the default value for languages to format.
+		/// </summary>
+		/// <param name="language">
+		/// The options language being used.
+		/// </param>
+		/// <returns>
+		/// A collection of <see cref="DocumentLanguages"/> indicating the default set of languages to format.
+		/// </returns>
+		public object CreateDefaultValue(string language)
+		{
+			var coll = new ObservableCollection<DocumentLanguages>();
+			foreach (var item in Enum.GetValues(typeof(DocumentLanguages)).Cast<DocumentLanguages>())
+			{
+				coll.Add(item);
+			}
+
+			return coll;
+		}
+	}
+}
--- DX_FormatOnSave/OptionSet.cs
+++ DX_FormatOnSave/OptionSet.cs
@@ -1,17 +1,18 @@
 ﻿using System;
-using DevExpress.CodeRush.Core;
-using DevExpress.CodeRush.Diagnostics.Internal;
+using System.Collections.ObjectModel;
+using DevExpress.CodeRush.Platform.Options;
 
 namespace DX_FormatOnSave
 {
 	/// <summary>
 	/// Provides a structure where plugin options can be saved.
 	/// </summary>
-	public class OptionSet
+	public class OptionSet : OptionsContainer
 	{
-		private const string SectionGeneral = "General";
-		private const string KeyEnabled = "Enabled";
-		private const string KeyLanguagesToFormat = "LanguagesToFormat";
+		/// <summary>
+		/// Backing store for <see cref="LanguagesToFormat"/>.
+		/// </summary>
+		private ObservableCollection<DocumentLanguages> _languagesToFormat;
 
 		/// <summary>
 		/// Initializes a new instance of the <see cref="DX_FormatOnSave.OptionSet" /> class
@@ -20,7 +21,7 @@ namespace DX_FormatOnSave
 		public OptionSet()
 		{
 			this.Enabled = true;
-			this.LanguagesToFormat = DocumentLanguages.CPlusPlus | DocumentLanguages.CSharp | DocumentLanguages.JavaScript | DocumentLanguages.VisualBasic | DocumentLanguages.Xml;
+			this.LanguagesToFormat = new ObservableCollection<DocumentLanguages>();
 		}
 
 		/// <summary>
@@ -29,7 +30,19 @@ namespace DX_FormatOnSave
 		/// <value>
 		/// <see langword="true" /> if the plugin is enabled; <see langword="false" /> if not.
 		/// </value>
-		public bool Enabled { get; set; }
+		[Option]
+		[OptionDefaultValue(true, "Neutral")]
+		public bool Enabled
+		{
+			get
+			{
+				return this.GetProperty(() => this.Enabled);
+			}
+			set
+			{
+				this.SetProperty<bool>(() => this.Enabled, value);
+			}
+		}
 
 		/// <summary>
 		/// Gets or sets the flags indicating the language types to format.
@@ -38,54 +51,18 @@ namespace DX_FormatOnSave
 		/// A <see cref="DocumentLanguages"/> flag combination indicating which
 		/// types of documents should be formatted on save.
 		/// </value>
-		public DocumentLanguages LanguagesToFormat { get; set; }
-
-		/// <summary>
-		/// Loads the options from storage.
-		/// </summary>
-		/// <param name="storage">A storage object from which options should be loaded.</param>
-		/// <returns>
-		/// A populated <see cref="DX_FormatOnSave.OptionSet"/> from the storage.
-		/// </returns>
-		/// <exception cref="System.ArgumentNullException">
-		/// Thrown if <paramref name="storage" /> is <see langword="null" />.
-		/// </exception>
-		public static OptionSet Load(DecoupledStorage storage)
+		[Option]
+		[OptionDefaultValue(typeof(LanguagesToFormatOptionsCreator), "Neutral")]
+		public ObservableCollection<DocumentLanguages> LanguagesToFormat
 		{
-			if (storage == null)
+			get
 			{
-				throw new ArgumentNullException("storage");
+				return this._languagesToFormat;
 			}
-			Log.SendMsg("Loading 'format on save' options.");
-			OptionSet options = new OptionSet();
-			OptionSet defaults = new OptionSet();
-			options.Enabled = storage.ReadBoolean(SectionGeneral, KeyEnabled, defaults.Enabled);
-			Log.SendBool("Enabled", options.Enabled);
-			options.LanguagesToFormat = storage.ReadEnum<DocumentLanguages>(SectionGeneral, KeyLanguagesToFormat, defaults.LanguagesToFormat);
-			Log.SendEnum("Languages to format", options.LanguagesToFormat);
-			return options;
-		}
-
-		/// <summary>
-		/// Saves the options to storage.
-		/// </summary>
-		/// <param name="storage">A storage object to which the options should be saved.</param>
-		/// <exception cref="System.ArgumentNullException">
-		/// Thrown if <paramref name="storage" /> is <see langword="null" />.
-		/// </exception>
-		public void Save(DecoupledStorage storage)
-		{
-			if (storage == null)
+			set
 			{
-				throw new ArgumentNullException("storage");
+				this.SetProperty(ref this._languagesToFormat, value, () => this.LanguagesToFormat);
 			}
-			Log.SendMsg("Saving 'format on save' options.");
-			storage.LanguageID = "";
-			storage.WriteBoolean(SectionGeneral, KeyEnabled, this.Enabled);
-			Log.SendBool("Enabled", this.Enabled);
-			storage.WriteEnum(SectionGeneral, KeyLanguagesToFormat, this.LanguagesToFormat);
-			Log.SendEnum("Languages to format", this.LanguagesToFormat);
-			storage.UpdateStorage();
 		}
 	}
 }
